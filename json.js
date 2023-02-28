8c37ba9413eed075f681815b8317d3d23de2d267
somaiya shahin <somaiyashahin484@gmail.com>
2023-03-01 00:17:45 +0600

    fetch 50 photos

---
+++ json.js
@@ -0,0 +1,28 @@
+const photo = () =>{
+    const URL = `https://jsonplaceholder.typicode.com/photos`;
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => getPhotos(data))
+}
+
+const getPhotos = (photos) =>{
+    console.log(photos);
+    photos.slice(0, 200).forEach((photo) => {
+        const {url, title, id} = photo;
+        console.log(photo)
+        const cardContainer = document.getElementById('card-container');
+        cardContainer.classList.add('col');
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="card">
+                <img src="${url} " class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Id:${id} </p>
+                    <h5 class="card-title">Title: ${title}</h5>
+                </div>
+            </div>
+        `;
+        cardContainer.appendChild(div)
+    });
+}
+photo()
